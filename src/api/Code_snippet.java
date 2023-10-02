e1d343c0a6df7dcfbd1589994af4b3b6e9ffabe3
Viceu <66227626+Viceu@users.noreply.github.com>
2023-10-01 22:04:31 -0400

    fixed print of JSONObject

--- src/api/Code_snippet.java
+++ src/api/Code_snippet.java
@@ -10,14 +10,14 @@ import java.util.Arrays;
 public class Code_snippet {
     private static final String API_TOKEN = "";
 
-    public String exampleCall() throws JSONException {
+    public static void exampleCall() throws JSONException {
 
         OkHttpClient client = new OkHttpClient().newBuilder()
                 .build();
         MediaType mediaType = MediaType.parse("text/plain");
         RequestBody body = RequestBody.create(mediaType, "");
         Request request = new Request.Builder()
-                .url("https://api.petfinder.com/v2/animals?type=dog&size=Small&coat=short")
+                .url("https://api.petfinder.com/v2/animals?type=dog&name=Zipadee")
                 .method("GET", body)
                 .addHeader("Authorization", API_TOKEN)
                 .build();
@@ -29,12 +29,17 @@ public class Code_snippet {
 
             if (responseBody.getInt("status_code") == 200) {
                 JSONObject animals = responseBody.getJSONObject("animals");
-                return animals.toString();
+                System.out.println(animals.toString(5)); // Print it with specified indentation
+                return;
             } else {
                 throw new RuntimeException(responseBody.getString("message"));
             }
         } catch (IOException | JSONException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public static void main(String[] args) {
+        exampleCall();
     }
 }
