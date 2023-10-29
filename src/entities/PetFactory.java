30ad830c9a6a5c2f140324d2e9be37b2b06545ac
kimcailyn <cailyn.kim@mail.utoronto.ca>
2023-10-29 16:44:30 -0400

    finished entities

---
+++ src/entities/PetFactory.java
@@ -0,0 +1,11 @@
+package entities;
+
+import java.util.List;
+import java.util.Map;
+
+public interface PetFactory {
+        Pet create(Integer petID, String organizationID, String URL, String name, List<String> colors,
+                   Map<String, String> breed, String species, List<String> coat, String age, Map<String,
+                Boolean> attributes, Map<String, Boolean> environment, String description, Boolean adoptable,
+                   Map<String, String> contact, String gender, String size);
+}
