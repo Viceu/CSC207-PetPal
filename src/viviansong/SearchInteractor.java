74bd4c453b1aa7e00b82dee13da6c7f2f2fbd184
viviansongg <88507197+viviansongg@users.noreply.github.com>
2023-11-13 16:45:40 -0500

    Implemented half of SearchInteractor, added SearchOutputBoundary, SearchOutputData and SearchPresenter to the same folder as the interactor to access classes directly and fixed a bug in SearchOutputBoundary where the parameter was a boolean instead of a string.

--- src/viviansong/SearchInteractor.java
+++ src/viviansong/SearchInteractor.java
@@ -1,6 +1,8 @@
 package viviansong;
 
-import java.util.Map;
+import entities.Pet;
+
+import java.util.*;
 
 public class SearchInteractor implements SearchInputBoundary {
     final SearchUserDataAccessInterface userDataAccessObject;
@@ -15,6 +17,57 @@ public class SearchInteractor implements SearchInputBoundary {
     @Override
     public void execute(SearchInputData searchInputData) {
         Map<String, String> requirements = searchInputData.getRequirements();
+
+        // getting pet objects
+        List<Pet> masterPetList = new ArrayList<>(userDataAccessObject.getPets);
+        Map<Integer, Pet> displaypetList;
+        for (Pet pet : masterPetList) {
+            if (!pet.getAdoptable()) {
+                continue;
+            } else if (masterPetList.contains(pet)) {
+                continue;
+            }
+            for (String req : requirements) {
+                if (pet.req != req.getValue) {
+                    masterPetList.remove(pet);
+                }
+            }
+        }
+
+        // getting ids
+
+        if (masterPetList.isEmpty()) {
+            searchPresenter.prepareFailView("No search results.");
+        } else {
+            SearchOutputData searchOutputData = new SearchOutputData(..., true);
+            searchPresenter.prepareSuccessView(searchOutputData);
+        }
+
+
+        /* PSEUDO-CODE:
+        List<Pet> masterPetList = userDataAccessObject.getPets.copy();
+        Map<Integer, Pet> displayPetList;
+        for pet in masterPetList:
+            if !pet.getAdoptable():
+                pass;
+            if pet in masterPetList.values():
+                pass;
+            for req in requirements:
+                if pet.req != req.value:
+                    masterPetList.remove(pet);
+        if masterPetList.empty():
+            searchPresenter.prepareFailView("No search results");
+        else:
+            # NOTE - create User entity?
+            User user = userDataAccessObject.get(searchInputData.getUsername());
+            SearchOutputData searchOutputData = new SearchOutputData()
+            searchPresenter.prepareSuccessView(searchOutputData);
+
+            # --- From CACoding:
+            LoginOutputData loginOutputData = new LoginOutputData(user.getName(), false);
+            loginPresenter.prepareSuccessView(loginOutputData);
+        * */
+
     }
 
 
