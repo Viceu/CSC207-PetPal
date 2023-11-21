053e7309627e788b4bc9ce223c60dacc55bcffbe
xi_ha <sabrina.xi@mai.utoronto.ca>
2023-11-21 17:57:39 -0500

    Created DisplayController and DisplayPresenter for the adopt use case

--- src/interface_adaptor/display/DisplayController.java
+++ src/interface_adaptor/display/DisplayController.java
@@ -1,2 +1,20 @@
-package interface_adaptor.display;public class DisplayController {
+package interface_adaptor.display;
+
+import entities.Pet;
+import use_case.display.DisplayInputBoundary;
+import use_case.display.DisplayInputData;
+
+import java.util.Map;
+
+public class DisplayController {
+    final DisplayInputBoundary displayUseCaseInteractor;
+    public DisplayController(DisplayInputBoundary displayUseCaseInteractor) {
+        this.displayUseCaseInteractor = displayUseCaseInteractor;
+    }
+
+    public void execute(Pet thisPet) {
+        DisplayInputData displayInputData = new DisplayInputData(thisPet);
+
+        displayUseCaseInteractor.execute(displayInputData);
+    }
 }
--- src/interface_adaptor/display/DisplayPresenter.java
+++ src/interface_adaptor/display/DisplayPresenter.java
@@ -1,2 +1,34 @@
-package interface_adaptor.display;public class DisplayPresenter {
+package interface_adaptor.display;
+
+import interface_adaptor.ViewManagerModel;
+import interface_adaptor.adopt_user_preview.AdoptUserPreviewState;
+import interface_adaptor.adopt_user_preview.AdoptUserPreviewViewModel;
+import use_case.display.*;
+
+public class DisplayPresenter implements DisplayOutputBoundary {
+    private final DisplayViewModel displayViewModel;
+    private final ViewManagerModel viewManagerModel;
+    private final AdoptUserPreviewViewModel adoptUserPreviewViewModel;
+
+    public DisplayPresenter(DisplayViewModel displayViewModel, ViewManagerModel viewManagerModel, AdoptUserPreviewViewModel adoptUserPreviewViewModel) {
+        this.displayViewModel = displayViewModel;
+        this.viewManagerModel = viewManagerModel;
+        this.adoptUserPreviewViewModel = adoptUserPreviewViewModel;
+    }
+    public void prepareSuccessView(DisplayOutputData petData){
+
+        AdoptUserPreviewState newState = adoptUserPreviewViewModel.getState();
+        newState.setPet(petData.getPet());
+        this.adoptUserPreviewViewModel.setState(newState);
+        adoptUserPreviewViewModel.firePropertyChanged();
+
+        viewManagerModel.setActiveView(adoptUserPreviewViewModel.getViewName());
+        viewManagerModel.firePropertyChanged();
+    }
+
+    public void prepareFailView(String failmessage) {
+        DisplayState displayState = displayViewModel.getState();
+        displayState.setSearchFailMessage(failmessage);
+        displayViewModel.firePropertyChanged();
+    }
 }
