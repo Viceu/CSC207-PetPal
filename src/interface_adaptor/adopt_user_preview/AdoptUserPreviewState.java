31c7f633a534af2479b32e5b325372cfaeb2386b
xi_ha <sabrina.xi@mai.utoronto.ca>
2023-11-21 17:40:36 -0500

    Created state and viewModel for adopt_user_preview to be used in adopt use case

--- src/interface_adaptor/adopt_user_preview/AdoptUserPreviewState.java
+++ src/interface_adaptor/adopt_user_preview/AdoptUserPreviewState.java
@@ -1,2 +1,21 @@
-package interface_adaptor.adopt_user_preview;public class AdoptUserPreviewState {
+package interface_adaptor.adopt_user_preview;
+
+import entities.Pet;
+
+public class AdoptUserPreviewState {
+    private Pet thisPet;
+    private String failMessage = null;
+
+    public void setPet(Pet pet) {
+        thisPet = pet;
+    }
+    public Pet getPet() {
+        return thisPet;
+    }
+    public void setSearchFailMessage(String errorMessage) {
+        failMessage = errorMessage;
+    }
+    public Object getPetError() {
+        return failMessage;
+    }
 }
