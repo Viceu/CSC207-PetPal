e1e10e5423a7d649d7be0df625deb2fa6a35fd98
viviansongg <88507197+viviansongg@users.noreply.github.com>
2023-11-27 14:42:57 -0500

    Deleted pseudocode from Search Interactor file.

--- src/use_case/search/SearchInteractor.java
+++ src/use_case/search/SearchInteractor.java
@@ -255,48 +255,5 @@ public class SearchInteractor implements SearchInputBoundary {
 
         }
 
-//        // getting pet objects
-//        List<Pet> masterPetList = new ArrayList<>(userDataAccessObject.getPets());
-//        Map<Integer, Pet> displayPetMap = new HashMap<>();
-//        for (Pet pet : masterPetList) {
-//            if (!pet.getAdoptable()) {
-//                continue;
-//            } else if (masterPetList.contains(pet)) {
-//                continue;
-//            }
-//            for (Map.Entry<String, String> req : requirements.entrySet()) {
-//                if ((req.equals("name")) && !(pet.getName().equals(req.getValue()))) {
-//                    masterPetList.remove(pet);
-//                } else if ((req.equals("colors")) && !(pet.getColors().equals(req.getValue()))) {
-//                    masterPetList.remove(pet);
-//                } else if ((req.equals("breed")) && !(pet.getBreed().equals(req.getValue()))) {
-//                    masterPetList.remove(pet);
-//                } else if ((req.equals("species")) && !(pet.getSpecies().equals(req.getValue()))) {
-//                    masterPetList.remove(pet);
-//                } else if ((req.equals("coat")) && !(pet.getCoat().equals(req.getValue()))) {
-//                    masterPetList.remove(pet);
-//                } else if ((req.equals("age")) && !(pet.getAge().equals(req.getValue()))) {
-//                    masterPetList.remove(pet);
-//                } else if ((req.equals("attributes")) && !(pet.getAttributes().equals(req.getValue()))) {
-//                    masterPetList.remove(pet);
-//                } else if ((req.equals("environment")) && !(pet.getEnvironment().equals(req.getValue()))) {
-//                    masterPetList.remove(pet);
-//                } else if ((req.equals("description")) && !(pet.getDescription().equals(req.getValue()))) {
-//                    masterPetList.remove(pet);
-//                } else if ((req.equals("contact")) && !(pet.getContact().equals(req.getValue()))) {
-//                    masterPetList.remove(pet);
-//                } else if ((req.equals("gender")) && !(pet.getGender().equals(req.getValue()))) {
-//                    masterPetList.remove(pet);
-//                } else if ((req.equals("size")) && !(pet.getSize().equals(req.getValue()))) {
-//                    masterPetList.remove(pet);
-//                }
-//            }
-//        }
-//        // putting pet and id in hashmap
-//        for (Pet pet1 : masterPetList) {
-//            displayPetMap.put(pet1.getPetID(), pet1);
-//        }
-
-
     }
 }
