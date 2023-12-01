91d15741c3e10477a7fbead2e903574421bb3cdc
kimcailyn <cailyn.kim@mail.utoronto.ca>
2023-12-01 16:55:56 -0500

    Added separate login process for organizations and users

--- src/interface_adaptor/login/LoginPresenter.java
+++ src/interface_adaptor/login/LoginPresenter.java
@@ -1,7 +1,13 @@
 package interface_adaptor.login;
 
+// for user homepage
 import interface_adaptor.home.HomeState;
 import interface_adaptor.home.HomeViewModel;
+
+// for organization homepage
+import interface_adaptor.organization.OrganizationHomeState;
+import interface_adaptor.organization.OrganizationHomeViewModel;
+
 import interface_adaptor.ViewManagerModel;
 import interface_adaptor.signup.SignupState;
 import use_case.login.LoginOutputBoundary;
@@ -12,26 +18,41 @@ import use_case.signup.SignupOutputData;
 public class LoginPresenter implements LoginOutputBoundary {
     private final LoginViewModel loginViewModel;
     private final HomeViewModel homeViewModel;
+    private final OrganizationViewModel orgViewModel;
     private ViewManagerModel viewManagerModel;
 
     public LoginPresenter(ViewManagerModel viewManagerModel,
-                          HomeViewModel loggedInViewModel,
+                          HomeViewModel homeViewModel, OrganizationViewModel orgViewModel,
                           LoginViewModel loginViewModel) {
         this.viewManagerModel = viewManagerModel;
         this.homeViewModel = homeViewModel;
+        this.orgViewModel = orgViewModel;
         this.loginViewModel = loginViewModel;
     }
 
+    
     @Override
     public void prepareSuccessView(LoginOutputData response) {
+        if (response.getType().equals("user"))
+        { // displays user homepage
+            HomeState homeState = homeViewModel.getState();
+            homeState.setUsername(response.getUsername());
+            this.homeViewModel.setState(homeState);
+            this.homeViewModel.firePropertyChanged();
 
-        HomeState homeState = homeViewModel.getState();
-        homeState.setUsername(response.getUsername());
-        this.homeViewModel.setState(homeState);
-        this.homeViewModel.firePropertyChanged();
+            this.viewManagerModel.setActiveView(homeViewModel.getViewName());
+            this.viewManagerModel.firePropertyChanged();
+        }
+        else if (response.getType().equals("organization"))
+        { // displays organization home page
+            OrganizationHomeState orgHomeState = orgViewModel.getState();
+            orgHomeState.setUsername(response.getUsername());
+            this.orgViewModel.setState(homeState);
+            this.orgViewModel.firePropertyChanged();
 
-        this.viewManagerModel.setActiveView(homeViewModel.getViewName());
-        this.viewManagerModel.firePropertyChanged();
+            this.viewManagerModel.setActiveView(homeViewModel.getViewName());
+            this.viewManagerModel.firePropertyChanged();
+        }
     }
 
     @Override
