1572489512f673476c089e912a7d5a780732940f
xi_ha <sabrina.xi@mai.utoronto.ca>
2023-12-02 16:41:30 -0500

    Created presenter, outputboundary, inputboundary, inputdata, outputdata, and controller for organization hoem page

--- src/interface_adaptor/org_adopt/OrgHomePresenter.java
+++ src/interface_adaptor/org_adopt/OrgHomePresenter.java
@@ -1,2 +1,16 @@
-package interface_adaptor.org_adopt;public class OrgHomePresenter {
+package interface_adaptor.org_adopt;
+
+import use_case.org_adopt.OrgHomeOutputBoundary;
+import use_case.org_adopt.OrgHomeOutputData;
+
+public class OrgHomePresenter implements OrgHomeOutputBoundary {
+
+    @Override
+    public void prepareSuccessView(OrgHomeOutputData result) {
+
+    }
+    @Override
+    public void prepareFailView(String failMessage) {
+
+    }
 }
--- src/use_case/org_adopt/OrgHomeOutputData.java
+++ src/use_case/org_adopt/OrgHomeOutputData.java
@@ -1,2 +1,18 @@
-package use_case.org_adopt;public class OrgHomeOutputData {
+package use_case.org_adopt;
+
+import entities.Requests;
+
+public class OrgHomeOutputData {
+    private final Requests request;
+    private boolean searchFail;
+
+    public OrgHomeOutputData(Requests request, boolean searchFail) {
+        this.request = request;
+        this.searchFail = searchFail;
+    }
+
+    public Requests getRequest() {
+        return request;
+    }
+
 }
