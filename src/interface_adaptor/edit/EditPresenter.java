afba571b8bdb77299563a843de1e355998713e67
mcjessxu <mcjess.xu@mail.utoronto.ca>
2023-12-02 21:22:13 -0500

    Created EditPresenter, EditState, EditView, and EditViewModel

---
+++ src/interface_adaptor/edit/EditPresenter.java
@@ -0,0 +1,39 @@
+package interface_adaptor.edit;
+
+import interface_adaptor.ViewManagerModel;
+import interface_adaptor.home.HomeState;
+import interface_adaptor.home.HomeViewModel;
+import use_case.edit.EditOutputBoundary;
+import use_case.edit.EditOutputData;
+
+public class EditPresenter implements EditOutputBoundary {
+
+    private final EditViewModel editViewModel;
+    private final HomeViewModel homeViewModel;
+    private ViewManagerModel viewManagerModel;
+
+    public EditPresenter(ViewManagerModel viewManagerModel, EditViewModel editViewModel, HomeViewModel homeViewModel) {
+        this.viewManagerModel = viewManagerModel;
+        this.editViewModel = editViewModel;
+        this.homeViewModel = homeViewModel;
+    }
+
+    @Override
+    public void prepareSuccessView(EditOutputData edited) {
+        // on success, switch to the home page
+        HomeState homeState = homeViewModel.getState();
+        homeState.setUsername(edited.getOwner());
+        this.homeViewModel.setState(homeState);
+        this.homeViewModel.firePropertyChanged();
+
+        this.viewManagerModel.setActiveView(homeViewModel.getViewName());
+        this.viewManagerModel.firePropertyChanged();
+    }
+
+    @Override
+    public void prepareFailView(String editFail) {
+        EditState editState = editViewModel.getState();
+        editState.setPetnameError(editFail);
+        editViewModel.firePropertyChanged();
+    }
+}
