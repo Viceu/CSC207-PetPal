c68347c6bac666cebe99a353b207fbb95208c228
viviansongg <88507197+viviansongg@users.noreply.github.com>
2023-12-02 20:29:32 -0500

    Created test files for the Organizations and Requests entities.

---
+++ test/entity/OrganizationsTest.java
@@ -0,0 +1,50 @@
+package entity;
+
+import entities.Organizations;
+import entities.Requests;
+import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+public class OrganizationsTest {
+    private Organizations organizations;
+    private ArrayList<Requests> requestList = new ArrayList<Requests>();
+
+    @BeforeEach
+    void init() {
+        organizations = new Organizations("fightingchanceranchandrecuse", "1234", "I love pets");
+    }
+
+    @Test
+    void getUsernameTest() {
+        assertEquals("fightingchanceranchandrecuse", organizations.getName());
+    }
+
+    @Test
+    void getPasswordTest() {
+        assertEquals("1234", organizations.getPassword());
+    }
+
+    @Test
+    void getBioTest() {
+        assertEquals("I love pets", organizations.getBio());
+    }
+
+    @Test
+    void addRequestTest() {
+
+    }
+
+    @Test
+    void acceptRequestTest() {
+
+    }
+
+    @Test
+    void rejectRequestTest() {
+
+    }
+
+}
